67561bbf9d963122201fe0f8c9c28d5f5e45f7d1
lolopindik <dimaluka2014@gmail.com>
2024-02-08 13:49:12 +0300

    Changed the appearance of the project

--- lib/main.dart
+++ lib/main.dart
@@ -26,109 +26,106 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: 
-      AppBar( 
+      appBar: AppBar(
         backgroundColor: const Color.fromARGB(143, 4, 150, 207),
         title: const Text(
-          'Columns', 
+          'Columns',
           textDirection: TextDirection.ltr,
           style: TextStyle(fontSize: 30),
-          ),
+        ),
         centerTitle: true,
       ),
-      body: Center(
-        child: 
-        Column(
-          children: <Widget>
-          [
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 60),
+        child: Center(
+          child: Column(children: <Widget>[
             Container(
-              width:MediaQuery.of(context).size.width * 0.7,
-              height: MediaQuery.of(context).size.height * 0.15,
-              margin: const EdgeInsets.all(10),
-              alignment: Alignment.center,
-              decoration: 
-              BoxDecoration(
-                color: const Color.fromARGB(255, 9, 40, 218),
-                borderRadius: BorderRadius.circular(16),
+                width: MediaQuery.of(context).size.width * 0.7,
+                height: MediaQuery.of(context).size.height * 0.1,
+                margin: const EdgeInsets.all(10),
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 9, 40, 218),
+                  borderRadius: BorderRadius.circular(30),
                 ),
-              child: const Text(
-                'Blue',
-                textAlign: TextAlign.center,
-                textDirection: TextDirection.ltr,
-                style: TextStyle(fontSize: 30,),
-                )
-            ),
+                child: const Text(
+                  'Blue',
+                  textAlign: TextAlign.center,
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    fontSize: 30,
+                  ),
+                )),
             Container(
-              width:MediaQuery.of(context).size.width * 0.7,
-              height: MediaQuery.of(context).size.height * 0.15,
-              margin: const EdgeInsets.all(10),
-              alignment: Alignment.center,
-              decoration: 
-              BoxDecoration(
-                color: const Color.fromARGB(255, 9, 218, 33),
-                borderRadius: BorderRadius.circular(16),
+                width: MediaQuery.of(context).size.width * 0.7,
+                height: MediaQuery.of(context).size.height * 0.1,
+                margin: const EdgeInsets.all(10),
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 9, 218, 33),
+                  borderRadius: BorderRadius.circular(30),
                 ),
-              child: const Text(
-                'Green',
-                textAlign: TextAlign.center,
-                textDirection: TextDirection.ltr,
-                style: TextStyle(fontSize: 30,),
-                )
-            ),
+                child: const Text(
+                  'Green',
+                  textAlign: TextAlign.center,
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    fontSize: 30,
+                  ),
+                )),
             Container(
-              width:MediaQuery.of(context).size.width * 0.7,
-              height: MediaQuery.of(context).size.height * 0.15,
-              margin: const EdgeInsets.all(10),
-              alignment: Alignment.center,
-              decoration: 
-              BoxDecoration(
-                color: const Color.fromARGB(255, 218, 9, 9),
-                borderRadius: BorderRadius.circular(16),
+                width: MediaQuery.of(context).size.width * 0.7,
+                height: MediaQuery.of(context).size.height * 0.1,
+                margin: const EdgeInsets.all(10),
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 218, 9, 9),
+                  borderRadius: BorderRadius.circular(30),
                 ),
-              child: const Text(
-                'Red',
-                textAlign: TextAlign.center,
-                textDirection: TextDirection.ltr,
-                style: TextStyle(fontSize: 30,),
-                )
-            ),
+                child: const Text(
+                  'Red',
+                  textAlign: TextAlign.center,
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    fontSize: 30,
+                  ),
+                )),
             Container(
-              width:MediaQuery.of(context).size.width * 0.7,
-              height: MediaQuery.of(context).size.height * 0.15,
-              margin: const EdgeInsets.all(10),
-              alignment: Alignment.center,
-              decoration: 
-              BoxDecoration(
-                color: const Color.fromARGB(255, 78, 4, 125),
-                borderRadius: BorderRadius.circular(16),
+                width: MediaQuery.of(context).size.width * 0.7,
+                height: MediaQuery.of(context).size.height * 0.1,
+                margin: const EdgeInsets.all(10),
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 78, 4, 125),
+                  borderRadius: BorderRadius.circular(30),
                 ),
-              child: const Text(
-                'Purple',
-                textAlign: TextAlign.center,
-                textDirection: TextDirection.ltr,
-                style: TextStyle(fontSize: 30,),
-                )
-            ),
+                child: const Text(
+                  'Purple',
+                  textAlign: TextAlign.center,
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.white,
+                  ),
+                )),
             Container(
-              width:MediaQuery.of(context).size.width * 0.7,
-              height: MediaQuery.of(context).size.height * 0.15,
-              margin: const EdgeInsets.all(10),
-              alignment: Alignment.center,
-              decoration: 
-              BoxDecoration(
-                color: const Color.fromARGB(255, 0, 0, 0),
-                borderRadius: BorderRadius.circular(16),
+                width: MediaQuery.of(context).size.width * 0.7,
+                height: MediaQuery.of(context).size.height * 0.1,
+                margin: const EdgeInsets.all(10),
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 0, 0, 0),
+                  borderRadius: BorderRadius.circular(30),
                 ),
-              child: const Text(
-                'Black',
-                textAlign: TextAlign.center,
-                textDirection: TextDirection.ltr,
-                style: TextStyle(fontSize: 30, color: Colors.white),
-                )
-            )
-          ]
+                child: const Text(
+                  'Black',
+                  textAlign: TextAlign.center,
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(fontSize: 30, color: Colors.white),
+                ))
+          ]),
         ),
-        ),
+      ),
     );
   }
 }
